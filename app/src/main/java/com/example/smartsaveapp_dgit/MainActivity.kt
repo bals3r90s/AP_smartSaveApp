b5ed137d8cb7ce432667755bdd7f4c81cf7498ec
Niklas <nilas.balserpp@web.de>
2024-06-18 12:15:20 +0200

    Gui try

--- app/src/main/java/com/example/smartsaveapp_dgit/MainActivity.kt
+++ app/src/main/java/com/example/smartsaveapp_dgit/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.smartsaveapp_dgit.ui.theme.SmartSaveApp_dgitTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SmartSaveApp_dgitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartSaveApp_dgitTheme {
-        Greeting("Android")
+        setContentView(R.layout.list_item_cash)
     }
 }
